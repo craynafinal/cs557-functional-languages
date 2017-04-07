27ffdf50c7e1d8e26f4ccb7a9ed745b9ebcb6917
Jong Seong Lee <jsl@pdx.edu>
2017-04-07 15:17:36 -0700

    ch1 practice update

--- practice/week1/ch1.hs
+++ practice/week1/ch1.hs
@@ -1,3 +1,11 @@
 product1 :: [Int] -> Int
 product1 [] = 1
 product1 l = (head l) * product1(tail l)
+
+qsort [] = []
+qsort (x:xs) = 	qsort smaller ++ [x] ++ qsort larger
+								where
+									smaller = [a | a <- xs, a <= x]
+									larger = [b | b <- xs, b > x]
+
+qsortrev l = reverse(qsort l)
