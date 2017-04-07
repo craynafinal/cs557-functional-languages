454b1366cb01a0626bf56ebc58bfc68f8db62202
Jong Seong Lee <jsl@pdx.edu>
2017-04-06 23:08:00 -0700

    hw1 q3 copy

--- hw1/q3.hs
+++ hw1/q3.hs
@@ -1,5 +1,8 @@
 powerOfTwo :: Int -> Integer
-powerOfTwo n = product (replicate n 2)
+powerOfTwo n = product (take n (repeat 2))
 
 logTwo :: Integer -> Int
 logTwo v = fromIntegral (last [x | x <- [1..v], (powerOfTwo (fromInteger x)) <= v])
+
+copy :: Int -> a -> [a]
+copy n x = take n (repeat x)
