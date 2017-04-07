313b0f43b0496ff4c31475fc9605caa8e2c5eca3
Jong Seong Lee <jsl@pdx.edu>
2017-04-06 23:37:45 -0700

    hw1 multiApply

--- hw1/q3.hs
+++ hw1/q3.hs
@@ -6,3 +6,7 @@ logTwo v = fromIntegral (last [x | x <- [1..v], (powerOfTwo (fromInteger x)) <= 
 
 copy :: Int -> a -> [a]
 copy n x = take n (repeat x)
+
+multiApply :: (a -> a) -> Int -> a -> a
+multiApply f n x 	| n > 0 = last (take (n + 1) (iterate f x))
+									| n <= 0 = x
