3790660572e4ffd867659c9820ecfc6be62f69c6
crayna <craynafinal@gmail.com>
2017-06-03 01:14:08 -0700

    moved scripts from main to lib

--- project/haskell-rest-api/app/Main.hs
+++ project/haskell-rest-api/app/Main.hs
@@ -1,50 +1,12 @@
-{-# LANGUAGE OverloadedStrings #-} 
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import Web.Scotty
 import Lib
-import Control.Monad.IO.Class
-import Data.Aeson (FromJSON, ToJSON, object, encode)
+
+import Web.Scotty
+
 import Database.PostgreSQL.Simple
-import Database.PostgreSQL.Simple.ToRow
-import Database.PostgreSQL.Simple.FromRow
-import Database.PostgreSQL.Simple.ToField
-import GHC.Generics
-
-server :: Connection -> ScottyM()
-server conn = do
-	post "/SumAPI/addNumber" $ do
-		item <- jsonData :: ActionM SumNum
-		newItem <- liftIO (sumNumber conn item updateNumberQuery)
-		json newItem
-	get "/SumAPI/showNumber" $ do
-		items <- liftIO (query_ conn selectNumberQuery :: IO [SumNum])
-		json items
-	get "/SumAPI/resetNumber" $ do
-		item <- liftIO (sumNumber conn (SumNum (Just 0)) resetNumberQuery)
-		json item
-		
-updateNumberQuery = "UPDATE sumNumber SET number = number + ? WHERE id = 1 returning number"
-resetNumberQuery = "UPDATE sumNumber SET number = ? WHERE id = 1 returning number"
-selectNumberQuery = "SELECT number FROM sumNumber"
-
-sumNumber :: Connection -> SumNum -> Query -> IO SumNum
-sumNumber conn item qString = do
-	[Only number] <- query conn qString item
-	return item { number = number }
-
-data SumNum = SumNum { number :: Maybe Int } deriving (Show, Generic)
-
-instance FromRow SumNum where
-	fromRow = SumNum <$> field
-
-instance ToRow SumNum where
-	toRow c = [toField $ number c]
-
-instance ToJSON SumNum
-instance FromJSON SumNum
 
 main :: IO ()
 main = do
--- project/haskell-rest-api/test/Spec.hs
+++ project/haskell-rest-api/test/Spec.hs
@@ -1,2 +1,21 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
+module Test where
+
+import           Test.Hspec
+import           Test.Hspec.Wai
+import           Test.Hspec.Wai.JSON
+
+import           Network.Wai (Application)
+import qualified Web.Scotty as S
+import           Data.Aeson (Value(..), object, (.=))
+
+--import 					 Lib
+import Main
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Verify that bassbull outputs the correct data" $ do
+    it "equals zero" $ do
+--      theSum <- getAtBatsSum "batting.csv"
+--      theSum `shouldBe` 4858210
+			"someFunc" `shouldBe` "someFunc"
